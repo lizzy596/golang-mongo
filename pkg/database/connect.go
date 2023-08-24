3816f658189edc901d3f6959f9e6f8e761e08f5b
lizzy596 <lizmeek123@gmail.com>
2023-08-24 16:35:21 -0500

    d

--- pkg/database/connect.go
+++ pkg/database/connect.go
@@ -3,9 +3,7 @@ package database
 import (
 	"context"
 	"fmt"
-	"log"
 	"time"
-	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 	"go.mongodb.org/mongo-driver/mongo/readpref"
@@ -14,24 +12,32 @@ import (
 func ConnectToDB() (*mongo.Client, error) {
 	// Replace with your MongoDB connection details
 	connectionString := "mongodb://mongo:27001,mongo:27002,mongo:27003/go-server"
-	client, err := mongo.NewClient(options.Client().ApplyURI(connectionString))
-	if err != nil {
-		log.Fatal(err)
-	}
-	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
-	err = client.Connect(ctx)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer client.Disconnect(ctx)
-	err = client.Ping(ctx, readpref.Primary())
-	if err != nil {
-		log.Fatal(err)
-	}
-	databases, err := client.ListDatabaseNames(ctx, bson.M{})
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println(databases)
+// Create a new MongoDB client
+client, err := mongo.NewClient(options.Client().ApplyURI(connectionString))
+if err != nil {
+	return nil, err
 }
+
+// Create a context with a timeout
+ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+defer cancel()
+
+// Connect to the MongoDB server
+err = client.Connect(ctx)
+if err != nil {
+	return nil, err
+}
+
+// Ping the server to check the connection status
+err = client.Ping(ctx, readpref.Primary())
+if err != nil {
+	return nil, err
+}
+
+fmt.Println("Connected to MongoDB!")
+
+// Return the client and error (if any)
+return client, nil
+}
+
 
